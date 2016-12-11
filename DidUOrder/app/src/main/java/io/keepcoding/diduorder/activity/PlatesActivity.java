e0ae516a6bde3144fdba1a4a0d19deb273567d88
amdaza <amdazacastillo@gmail.com>
2016-12-11 13:00:23 +0100

    Create plates activity template

---
+++ DidUOrder/app/src/main/java/io/keepcoding/diduorder/activity/PlatesActivity.java
@@ -0,0 +1,15 @@
+package io.keepcoding.diduorder.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import io.keepcoding.diduorder.R;
+
+public class PlatesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plates);
+    }
+}
